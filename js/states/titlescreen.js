7685b9ce6c950ada007f87c68781c31f06952dd8
Kem Muhammad <siddiqtechnologies@yahoo.com>
2018-04-29 11:22:48 -0400

    New Changes

--- js/states/titlescreen.js
+++ js/states/titlescreen.js
@@ -6,6 +6,8 @@ var TitleScreen = {
         var playBtn = this.game.add.sprite(this.game.world.centerX,this.game.world.centerY+375,'playBtn');
         // var playBtn = this.game.add.sprite(this.game.world.centerX,1000,'playBtn');
         // playBtn.scale.setTo(scaleRatio, scaleRatio);
+        var style = {font: "35px Arial", fill: "#000"};
+        this.game.add.text(this.game.world.centerX, this.game.world.centerY, 'Tank Run', style);
         playBtn.anchor.setTo(0.5);
         playBtn.inputEnabled = true;
         playBtn.events.onInputDown.add(function(){
